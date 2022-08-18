0507b0fde9375ab513402d73001a9d48fbca2525
Gavin Lochtefeld <gavin.loch@gmail.com>
2022-08-17 21:04:11 -0500

    messin about

--- core/main.php
+++ core/main.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace pverb\core {
+
 class PVFailed extends Exception {
     public function __construct($message, $code=0, Throwable $previous=null) {
         parent::__construct($message, $code, $previous);
@@ -9,8 +11,32 @@ class PVFailed extends Exception {
     }
 }
 
+class Runner {
+    private array $test_callbacks;
+    public function add_test($test) {}
+    public function nest_test($test) {}
 
-function describe( string $state, $action ) {
 }
-    
+
+class Expectation {
+    private $expected;
+    private $actual;
+    private $result;
+    public function expect($state) {
+        $this->actual = $state;
+    }
+    public function actual($state) {
+        $this->expected = $state;
+    }
+    public function compare() {
+        return $this->actual === $this->expected ? true : false;
+    }
+}
+
+
+function describe( string $state, $action ) { }
+function it( string $state, $action) { }
+
+}
+  
 ?>
